bb86a7cfc5e0f5d88b123c323713b0a6bafb8bb8
danny olum <dannyolumese@gmail.com>
2016-11-08 00:26:01 +0000

    Banking Application

---
+++ BankAccount/src/com/bankaccount/Main.java
@@ -0,0 +1,29 @@
+package com.bankaccount;
+
+
+
+
+/**
+ * Created by dannyolu on 07/11/2016.
+ */
+public class Main {
+
+    public  static void main (String args []){
+
+        AccountDetails accountdetails = new AccountDetails ();
+        accountdetails.withDrawMoney();
+        accountdetails.payMoney();
+        accountdetails.payBills();
+        accountdetails.changeDetails();
+
+
+
+
+          }
+
+
+    }
+
+
+
+
