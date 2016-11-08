af403cff0e3f943bbbb27efa48fa9280e170f301
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-11-08 20:45:30 +0000

    This is update we made on 08-11-2016

---
+++ FirstProject/src/test/UsElectionResult.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by dotcom on 08/11/16.
+ */
+public class UsElectionResult {
+
+
+    public void checkElectionResult(){
+        System.out.println("Hello Clinton");
+    }
+}
