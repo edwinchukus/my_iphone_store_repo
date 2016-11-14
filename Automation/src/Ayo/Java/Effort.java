9a03e3fbf64fc3e771cc1ede336df47e440b2449
Ayo Lawal <mayobamie@gmail.com>
2016-11-14 02:02:31 +0000

    update code

--- Automation/src/Ayo/Java/Effort.java
+++ Automation/src/Ayo/Java/Effort.java
@@ -4,19 +4,22 @@ package Ayo.Java;
  * Created by ASUS on 14/11/2016.
  */
 public class Effort {
-    System.out.println("Automation");
+
+    public static void main(String [] args)
+    System.out.print("Automation");
+
 
     int myArrays[] = {10, 20, 30};
 		System.out.println(myArrays.length);
 		System.out.println(myArrays[0]);
-    System.out.println("Int")
+    System.out.print("Int")
 
 
     var chaitanya =
             "firstName" : "Ayobami",
             "lastName" : "LAWAL",
-            "age" :  "00";
-    System.out.println( " Basic Info");
+            "age" :"00";
+    System.out.println("Basic Info");
 }
 
 
