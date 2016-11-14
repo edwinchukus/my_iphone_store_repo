e6ef20602c832369e7a55602d959dd4a769833a1
Ayo Lawal <mayobamie@gmail.com>
2016-11-14 14:02:54 +0000

    Update stuffs

--- Automation/src/Ayo/Java/Effort.java
+++ Automation/src/Ayo/Java/Effort.java
@@ -5,22 +5,22 @@ package Ayo.Java;
  */
 public class Effort {
 
-    public static void main(String [] args)
-    System.out.print("Automation");
+    public static void main(String[] args) {
+        System.out.println("Automation");
 
 
-    int myArrays[] = {10, 20, 30};
-		System.out.println(myArrays.length);
-		System.out.println(myArrays[0]);
-    System.out.print("Int")
+        int myArrays[] = {10, 20, 30};
+        System.out.println(myArrays.length);
+        System.out.println(myArrays[0]);
+        System.out.print("Int")
 
 
-    var chaitanya =
-            "firstName" : "Ayobami",
-            "lastName" : "LAWAL",
-            "age" :"00";
-    System.out.println("Basic Info");
+        var chaitanya =
+                "firstName" "Ayobami",
+                "lastName" :"LAWAL",
+                "age":"00";
+        System.out.println("Basic Info");
+    }
 }
-
 
 
