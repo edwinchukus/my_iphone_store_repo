e0f2234dcdcc98c96d9d77c378869140e1a77b71
Ayo Lawal <mayobamie@gmail.com>
2016-11-15 18:14:00 +0000

    Practice

---
+++ Premier League/src/com/test/Main.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import javax.naming.Name;
+import java.util.Scanner;
+import java.util.jar.Attributes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+     
+        System.out.println("Arsenal");
+
+
+    }
+}
