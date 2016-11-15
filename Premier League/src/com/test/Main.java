89a6240246f84d86b98580f52589df069736f308
Ayo Lawal <mayobamie@gmail.com>
2016-11-15 18:30:39 +0000

    Update Priject

--- Premier League/src/com/test/Main.java
+++ Premier League/src/com/test/Main.java
@@ -8,8 +8,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-     
+
         System.out.println("Arsenal");
+        System.out.println("Chelsea");
+        System.out.println("Manchester United");
+        System.out.println("Manchester City");
+        System.out.println("Liverpool");
+
+
 
 
     }
