f8ec91bb415b03aebca5e4b6c676eff82ffb57af
Kelvin Boakye <kkboakye45@yahoo.co.uk>
2016-11-15 19:34:45 +0000

    Added For loop file to the project

---
+++ ClassProject/src/loops/ForLoops.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class ForLoops {
+
+	
+		public static void main(String[] args) {
+		    for (int x = 12; x<=70; x=x+1){
+			System.out.println("Welcome to BMW WORLD" + x);
+				}
+			}
+
+		}
