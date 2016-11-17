75a5db0e9b5f5c47d2158259bd454696b2d0518e
danny olum <dannyolumese@gmail.com>
2016-11-17 18:19:37 +0000

    Property

---
+++ Property/src/wednesday/HouseWed.java
@@ -0,0 +1,57 @@
+package wednesday;
+
+/**
+ * Created by dannyolu on 17/11/2016.
+ */
+public class HouseWed {
+
+
+    private String houseType = "Flat";
+    private int houseNo = 50;
+    private double housePrice = 50000.00;
+    protected char houseGrade;
+    boolean threeBedRoom = true;
+
+
+    public void RentingHouse() {
+        System.out.println(houseType);
+    }
+
+
+    public void sellingMyHouse() {
+        if (housePrice >= 50000.00) {
+            System.out.println("I should sell");
+        } else {
+            System.out.println("Don't sell, wait after the BritExit");
+
+        }
+    }
+
+
+    public void GradingHouse() {
+        double houseGrade = 50000.00;
+
+    }
+
+
+   public void sethouseNo(int houseNo) {
+      this.houseNo = houseNo;
+   }
+//        if (houseNo > 60) {
+//          System.out.println("Good to change the house no");
+//       } else {
+//            System.out.println("leave it as it is");
+//        }
+
+    public String getHouseType() {
+        return houseType;
+    }
+
+    public double getHousePrice() {
+        return housePrice;
+    }
+    public int gethouseNo() {
+        return houseNo;
+    }
+
+}
