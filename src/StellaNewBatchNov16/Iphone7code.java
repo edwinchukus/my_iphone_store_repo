25e2568710f871b14f291314dde711ead871ffbf
Stella <sa579873@gmail.com>
2016-11-17 07:08:21 +0000

    stella update

---
+++ src/StellaNewBatchNov16/Iphone7code.java
@@ -0,0 +1,10 @@
+package StellaNewBatchNov16;
+
+/**
+ * Created by Stella on 14/11/2016.
+ */
+public class Iphone7code {
+        public static void main(String[] args)
+        {System.out.println("Stella loves Iphones!!");}}
+
+
