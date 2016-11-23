c460ccb03e2ccb51ae6706d3242ca7265d4ed718
Lanre Sarumi <sarumilanre@gmail.com>
2016-11-23 18:32:56 +0000

    trial

---
+++ Collaboration1/src/com/collaboration1/Main.java
@@ -0,0 +1,17 @@
+package com.collaboration1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Danny's
+
+
+          String  Danny;
+        
+        System.out.println("Danny");
+
+
+        }
+
+    }
+
