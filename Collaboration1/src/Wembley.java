08315bafe83fc4eb63e1d4207d3c947262ab9ce4
Lanre Sarumi <sarumilanre@gmail.com>
2016-11-24 04:17:25 +0000

    Created new class

---
+++ Collaboration1/src/Wembley.java
@@ -0,0 +1,8 @@
+/**
+ * Created by LanreSarumi on 24/11/2016.
+ */
+public class Wembley {
+    public static void main(String [] args){
+        System.out.println("Please enter favourite team name");
+    }
+}
--- Collaboration1/src/com/collaboration1/Main.java
+++ Collaboration1/src/com/collaboration1/Main.java
@@ -3,12 +3,8 @@ package com.collaboration1;
 public class Main {
 
     public static void main(String[] args) {
-	// Danny's
 
-
-          String  Danny;
-        
-        System.out.println("Danny");
+       // System.out.println("Danny");
 
 
         }
