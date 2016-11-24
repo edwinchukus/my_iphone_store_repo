5481428cf1cd0486401bfbc6eafa7c91bc0d4578
Ayo Lawal <mayobamie@gmail.com>
2016-11-24 19:36:27 +0000

    New Project

--- Capital Project/src/CompanyStaff/EngineeringDept.java
+++ Capital Project/src/CompanyStaff/EngineeringDept.java
@@ -4,4 +4,15 @@ package CompanyStaff;
  * Created by ASUS on 24/11/2016.
  */
 public class EngineeringDept {
+
+    public static void main(String[] args)
+
+    String name = "Ayobami";
+    int letter = name.codePointAt(3);
+        System.out.println(letter);
+
 }
+
+
+}
+
--- Premier League/src/Football/Players.java
+++ Premier League/src/Football/Players.java
@@ -5,23 +5,17 @@ package Football;
  */
 public class Players {
 
-    static static void main (String [] args){
+    static void main(String args)[])
+    {
 
+        System.out.println("Please enter you Age");
 
-        System.out.print("Please enter you Age");
-
-        int PlayerAge = 32;
+    int PlayerAge = 32;
         System.out.println("Congratulations,Welcome to Premier League");
 
-        else (PlayerAge>32);
-        System.out.println("Sorry you are too Old to kick football");
-
-        else if (PlayerAge <=32);
+    else if (PlayerAge <=32);
         "Congratulations,Welcome to Premier League");
 
-
-
-
-
+    } else (PlayerAge>32);
+        System.out.println("Sorry you are too Old to kick football");
     }
-}
