48fe7c0cca922921034351987e2594cb0a4b9dd3
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-11-29 22:13:10 +0000

    Updated codes on enum and error handling in Java

---
+++ Test3Project/src/program/constants/Circle.java
@@ -0,0 +1,26 @@
+package program.constants;
+
+/**
+ * Created by dotcom on 29/11/16.
+ */
+public class Circle {
+
+    private static final float PI = 3.14f;
+    private static float name = 74694f;
+    private double radius = 5.6;
+    private static final double MAXTRAINSPEED = 40d;
+
+
+
+
+    public double computeCircleArea(){
+        double area = PI * radius * radius;
+        return area;
+    }
+
+
+    public void computeCircumference(){
+
+    }
+
+}
