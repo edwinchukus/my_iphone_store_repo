ffcd37e88dc345b88d4989da7443aa7923a9a309
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-12-01 22:44:44 +0000

    Updated and added all the session  codes

---
+++ Test3Project/src/object/oriented/runner/oop/TestRunner.java
@@ -0,0 +1,20 @@
+package object.oriented.runner.oop;
+
+import object.oriented.encapsulation.Dove;
+
+/**
+ * Created by dotcom on 01/12/16.
+ */
+public class TestRunner {
+
+
+
+    public static void main(String [] args){
+        Dove dove = new Dove();
+
+
+
+
+
+    }
+}
