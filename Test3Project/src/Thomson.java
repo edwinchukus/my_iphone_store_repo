62070cd41f70c4310f77f40b63f9a5be291eb3ed
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-12-06 21:56:48 +0000

    Deleted for accidental files

--- Test3Project/src/Thomson.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by dotcom on 06/12/16.
- */
-public class Thomson {
-
-    public Thomson() {
-        System.out.println("Hello");
-    }
-}
--- Test3Project/src/program/constants/test.java
+++
@@ -1,7 +0,0 @@
-package program.constants;
-
-/**
- * Created by dotcom on 06/12/16.
- */
-public class test {
-}
