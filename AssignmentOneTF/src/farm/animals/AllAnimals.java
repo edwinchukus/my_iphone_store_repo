5c3611a3b5f672f6524fb3f12f619e148898b001
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-12-08 22:18:26 +0000

    Review and fixes for Tunde

--- AssignmentOneTF/src/farm/animals/AllAnimals.java
+++ AssignmentOneTF/src/farm/animals/AllAnimals.java
@@ -10,56 +10,39 @@ public class AllAnimals {
     enum AnimalActivity{walk, run, jump};
 
     public void callAnimalName(AnimalActivity activity){
-        System.out.println("Dear customer: We have different animals for sale. Type either Cattle, Sheep or Goat please.");
-        Scanner scanner = new Scanner(System.in);
-        String animalName = scanner.nextLine();
+        String animalName = interviewBuyer();
 
         if(animalName.equalsIgnoreCase("cattle")){
-            System.out.println("My name is a Cattle and I can eat");
-            System.out.println("Type an activity");
-            Scanner scan = new Scanner(System.in);
-            String active = scan.nextLine();
-            if(active.equalsIgnoreCase("walk") && activity.equals(AnimalActivity.walk)){
-                System.out.println("Yes! I can walk");
-            }else  if(active.equalsIgnoreCase("run") && activity.equals(AnimalActivity.run)) {
-                System.out.println("Yes! I can run");
-            }else  if(active.equalsIgnoreCase("jump") && activity.equals(AnimalActivity.jump)) {
-                System.out.println("Yes! I can jump");
-            }else {
-                System.out.println("sorry! I can't do that");
-            }
+            getAnimalActions(activity, "My name is a Cattle and I can eat");
         }else if(animalName.equalsIgnoreCase("sheep")){
-            System.out.println("My name is a Sheep and I can eat");
-            System.out.println("Type an activity");
-            Scanner scan = new Scanner(System.in);
-            String active = scan.nextLine();
-            if(active.equalsIgnoreCase("walk") && activity.equals(AnimalActivity.walk)){
-                System.out.println("Yes! I can walk");
-            }else  if(active.equalsIgnoreCase("run") && activity.equals(AnimalActivity.run)) {
-                System.out.println("Yes! I can run");
-            }else  if(active.equalsIgnoreCase("jump") && activity.equals(AnimalActivity.jump)) {
-                System.out.println("Yes! I can jump");
-            }else {
-                System.out.println("sorry! I can't do that");
-            }
+            getAnimalActions(activity, "My name is a Sheep and I can eat");
         }else if(animalName.equalsIgnoreCase("goat")){
-            System.out.println("My name is a Goat and I can eat");
-            System.out.println("Type an activity");
-            Scanner scan = new Scanner(System.in);
-            String active = scan.nextLine();
-            if(active.equalsIgnoreCase("walk") && activity.equals(AnimalActivity.walk)){
-                System.out.println("Yes! I can walk");
-            }else  if(active.equalsIgnoreCase("run") && activity.equals(AnimalActivity.run)) {
-                System.out.println("Yes! I can run");
-            }else  if(active.equalsIgnoreCase("jump") && activity.equals(AnimalActivity.jump)) {
-                System.out.println("Yes! I can jump");
-            }else {
-                System.out.println("sorry! I can't do that");
-            }
+            getAnimalActions(activity, "My name is a Goat and I can eat");
         }else{
             System.out.println("Animal not found");
         }
+    }
 
+    private void getAnimalActions(AnimalActivity activity, String s) {
+        System.out.println(s);
+        System.out.println("Type an activity");
+        Scanner scan = new Scanner(System.in);
+        String active = scan.nextLine();
+        if (active.equalsIgnoreCase("walk") && activity.equals(AnimalActivity.walk)) {
+            System.out.println("Yes! I can walk");
+        } else if (active.equalsIgnoreCase("run") && activity.equals(AnimalActivity.run)) {
+            System.out.println("Yes! I can run");
+        } else if (active.equalsIgnoreCase("jump") && activity.equals(AnimalActivity.jump)) {
+            System.out.println("Yes! I can jump");
+        } else {
+            System.out.println("sorry! I can't do that");
+        }
+    }
+
+    private String interviewBuyer() {
+        System.out.println("Dear customer: We have different animals for sale. Type either Cattle, Sheep or Goat please.");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
 }
