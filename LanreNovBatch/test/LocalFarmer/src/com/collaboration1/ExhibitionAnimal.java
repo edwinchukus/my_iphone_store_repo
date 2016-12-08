dba0dbb21d2cbe05f7dbdc17dd5daba38227e82d
Lanre Sarumi <sarumilanre@gmail.com>
2016-12-08 19:56:16 +0000

    AssignmentOneLanre

---
+++ LanreNovBatch/test/LocalFarmer/src/com/collaboration1/ExhibitionAnimal.java
@@ -0,0 +1,53 @@
+package com.collaboration1;
+
+
+import java.util.Scanner;
+
+/**
+ * Created by LanreSarumi on 08/12/2016.
+ */
+public class ExhibitionAnimal {
+    private String FarmAnimals[];
+
+    public ExhibitionAnimal() {
+        FarmAnimals = new String[3];
+        FarmAnimals[0] = "Goat";
+        FarmAnimals[1] = "Cattle";
+        FarmAnimals[2] = "Sheep";
+    }
+
+
+    public static void selectFarmAnimal() {
+        System.out.println("Please select farm Animal");
+        Scanner scanner = new Scanner(System.in);
+        String FarmAnimal = scanner.nextLine();
+
+
+        if (FarmAnimal.contains("Goat")) {
+            System.out.println(FarmAnimal + " is jumping");
+            System.out.println(FarmAnimal + " is walking");
+            System.out.println(FarmAnimal + " is running");
+            System.out.println(FarmAnimal + " is eating");
+
+
+        }else if (FarmAnimal.contains("Sheep"))  {
+            System.out.println(FarmAnimal + " is jumping");
+            System.out.println(FarmAnimal + " is walking");
+            System.out.println(FarmAnimal + " is running");
+            System.out.println(FarmAnimal + " is eating");
+
+
+        }else if (FarmAnimal.contains("Cattle")) {
+        System.out.println(FarmAnimal + " is jumping");
+        System.out.println(FarmAnimal + " is walking");
+        System.out.println(FarmAnimal + " is running");
+        System.out.println(FarmAnimal + " is eating");
+
+        }
+    }
+}
+
+
+
+
+
---
+++ LanreNovBatch/test/LocalFarmer/src/com/collaboration1/Main.java
@@ -0,0 +1,13 @@
+package com.collaboration1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       ExhibitionAnimal FarmAnimal = new ExhibitionAnimal();
+        FarmAnimal.selectFarmAnimal();
+    }
+
+
+    }
