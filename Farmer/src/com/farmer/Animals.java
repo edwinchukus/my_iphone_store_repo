ddad79df8368f2b0e0471e13004c2441bc3f3381
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-12-08 22:06:21 +0000

    Reviews and fixes

--- Farmer/src/com/farmer/Animals.java
+++ Farmer/src/com/farmer/Animals.java
@@ -6,18 +6,11 @@ package com.farmer;
 public class Animals {
 
     private String animalName;
-    private String buyerDemand;
-    private String animalEat;
-    private String animalRun;
-    private String animalWalk;
-    private String animalJump;
-    private String Cattle;
-    private String Sheep;
+
 
 
     public Animals(String animalName) {
         this.animalName = animalName;
-
     }
 
     public String getAnimalName() {
@@ -25,33 +18,33 @@ public class Animals {
     }
 
     public void animalEating() {
-        System.out.println("Eating");
+        System.out.println("I am a "+animalName+" and I can eat");
     }
 
     public void animalRuning() {
-        System.out.println("Running");
+        System.out.println("I am a "+animalName+" and I can run");
     }
 
     public void animalWalking() {
-        System.out.println("Walking");
+        System.out.println("I am a "+animalName+" and I can walk");
     }
 
     public void animaJumping() {
-        System.out.println("Jumping");
+        System.out.println("I am a "+animalName+" and I can jump");
     }
 
 
-    public void buyerVisitFarm() {
-        if (buyerDemand == Cattle) {
-            System.out.println("My name is Cattle and i can talk");
-        } else if (buyerDemand == Cattle) {
-            System.out.println("My name is Cattle and i can run");
-        } else if (buyerDemand == Cattle) {
-            System.out.println("My name is Cattle and i can walk");
-        } else if (buyerDemand == Cattle) {
-            System.out.println("My name is Cattle and i can jump");
-        } else {
-            System.out.println("Other action");
+    public void commandAnimal(String action) {
+        if(action.equalsIgnoreCase("run")) {
+            animalRuning();
+        }else if(action.equalsIgnoreCase("jump")){
+            animaJumping();
+        }else if(action.equalsIgnoreCase("walk")){
+            animalWalking();
+        }else if(action.equalsIgnoreCase("eat")){
+            animalEating();
+        }else{
+            System.out.println("There is no such action here");
         }
     }
 }
---
+++ Farmer/src/com/farmer/FarmRunner.java
@@ -0,0 +1,18 @@
+package com.farmer;
+
+import java.util.Scanner;
+
+/**
+ * Created by dotcom on 08/12/16.
+ */
+public class FarmRunner {
+
+
+    public static void main(String args []){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("****WElcome to Animal run by George Owell; Please ");
+
+    }
+}
