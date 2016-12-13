c91442fbce09d72029c951a830f6ac52607ecdf3
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-12-13 22:38:52 +0000

    Fixes

---
+++ Robot/src/com/company/interfacepack/TescoExtra.java
@@ -0,0 +1,40 @@
+package com.company.interfacepack;
+
+/**
+ * Created by dotcom on 13/12/16.
+ */
+public class TescoExtra implements SunderlandCityCouncil{
+
+    private double accountBalance ;
+
+
+
+    @Override
+    public void getFreshFarmProduce() {
+        System.out.println("Yes, We get fresh produce from Yorkshire");
+    }
+
+    @Override
+    public String showStoreName() {
+        String tescoStoreName = "Tesco Extra";
+        return tescoStoreName;
+    }
+
+    @Override
+    public long getShopAccrediationNumber(String company) {
+        if(company.equalsIgnoreCase("city council") && company != null){
+            return 981616;
+        }
+        return 0;
+    }
+
+    @Override
+    public void payTaxToSunderlandCityCouncil() {
+        accountBalance = accountBalance+ 1000;
+    }
+
+    @Override
+    public boolean doYouHaveCoolingVan() {
+        return true;
+    }
+}
