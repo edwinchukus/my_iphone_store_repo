7f95787c74db3de5690d5627be2e954e87af6048
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-12-13 21:57:27 +0000

    Updates

---
+++ Test3Project/src/object/oriented/interfacepack/Lidl.java
@@ -0,0 +1,32 @@
+package object.oriented.interfacepack;
+
+/**
+ * Created by dotcom on 13/12/16.
+ */
+public class Lidl implements SunderlandCityCouncil {
+
+    @Override
+    public void getFreshFarmProduce() {
+        System.out.println(COUNCILNAME);
+    }
+
+    @Override
+    public String showStoreName() {
+        return null;
+    }
+
+    @Override
+    public long getShopAccrediationNumber(String company) {
+        return 0;
+    }
+
+    @Override
+    public void payTaxToSunderlandCityCouncil() {
+
+    }
+
+    @Override
+    public boolean doYouHaveCoolingVan() {
+        return false;
+    }
+}
---
+++ Test3Project/src/object/oriented/runner/oop/InterRunner.java
@@ -0,0 +1,16 @@
+package object.oriented.runner.oop;
+
+import object.oriented.interfacepack.TescoExtra;
+
+/**
+ * Created by dotcom on 13/12/16.
+ */
+public class InterRunner {
+
+
+    public static void main(String args []){
+        TescoExtra tescoExtra = new TescoExtra();
+        tescoExtra.getFreshFarmProduce();
+
+    }
+}
