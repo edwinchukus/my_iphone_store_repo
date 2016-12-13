ea69bc86f12ef71dcd533d6b6a96c9800bf77ef1
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-12-13 22:29:48 +0000

    Updated

---
+++ Test2Project/src/interfacepack/InterRunner.java
@@ -0,0 +1,15 @@
+package interfacepack;
+
+/**
+ * Created by dotcom on 13/12/16.
+ */
+public class InterRunner {
+
+
+    public static void main(String args []){
+
+        TescoExtra tescoExtra = new TescoExtra();
+        tescoExtra.getFreshFarmProduce();
+
+    }
+}
---
+++ Test2Project/src/interfacepack/SunderlandCityCouncil.java
@@ -0,0 +1,25 @@
+package interfacepack;
+
+
+
+/**
+ * Created by dotcom on 13/12/16.
+ */
+public interface SunderlandCityCouncil {
+
+
+
+    String COUNCILNAME = "Sunderland CIty Council";
+
+    void getFreshFarmProduce();
+
+    String showStoreName();
+
+    long getShopAccrediationNumber(String company);
+
+    void payTaxToSunderlandCityCouncil();
+
+    boolean doYouHaveCoolingVan();
+
+
+}
